0617907124f519d82ed444829f94dfca45a0b2be
Fenomenon199 <afeldman2025@kehillahstudent.org>
2024-11-13 12:11:55 -0800

    ex2.py

--- ex1.py
+++ ex1.py
@@ -1,1 +1,8 @@
 # Use turtle graphics to make an infinite spiral
+
+from turtle import *
+i=1
+while true:
+    forward(i)
+    right(90)
+    i+=1
--- ex2.py
+++ ex2.py
@@ -1,1 +1,7 @@
 # make infinite circles, start with a small circle, then draw a bigger circle around it
+from turtle import * 
+r= 7
+x= 5
+while True: 
+    circle(x)
+    x= x+2
