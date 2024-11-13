2eb1eacf99836881c8a2265e0ed808b3b14a5151
Fenomenon199 <afeldman2025@kehillahstudent.org>
2024-11-13 12:22:24 -0800

    ex3.py

--- ex3.py
+++ ex3.py
@@ -1,1 +1,9 @@
 # write a factorial function, given n, you return n!
+def function(n): 
+    counter=1 
+    product=1 
+    while counter < n:
+        product=product*counter 
+        counter=counter+1
+    return product*counter 
+print(function(5))
